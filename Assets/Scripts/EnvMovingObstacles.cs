ec6fa9627f28b3fca0e5198102c490a6b69a1317
zachlo89 <zachlo89@gmail.com>
2021-02-03 13:39:44 -0800

    added scripts

---
+++ Assets/Scripts/EnvMovingObstacles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvMovingObstacles : MonoBehaviour
+{
+    [SerializeField] private GameObject cylPrefab, cubePrefab, spherePrefab;
+
+
+    void Start()
+    {
+        if (cylPrefab != null)
+        {
+            cylPrefab = GetComponent<GameObject>();
+        }
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _spawnPoint;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
