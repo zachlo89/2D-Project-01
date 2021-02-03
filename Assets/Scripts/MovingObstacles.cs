2a884f52f2cd401b0619b84caae43b312e4a8754
zachlo89 <zachlo89@gmail.com>
2021-02-03 14:28:00 -0800

    Spawn manager work

--- Assets/Scripts/MovingObstacles.cs
+++ Assets/Scripts/MovingObstacles.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnvMovingObstacles : MonoBehaviour
+public class MovingObstacles : MonoBehaviour
 {
     [SerializeField] private GameObject cylPrefab, cubePrefab, spherePrefab;
 
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _spawnPoint;
+    [SerializeField] Vector3 _spawnPoint = new Vector3();
+
+    public List<GameObject> obstacles = new List<GameObject>();
+
+    [SerializeField] private float _objSpawnInterval = 3.0f;
     
 
     void Start()
     {
-        
+        //foreach (var item in obstacles)
+        //{
+
+        //}
     }
 
     void Update()
     {
-        
+        StartCoroutine(ObstacleSpawnRoutine());
     }
+
+    // spawn obstacles every 3 seconds
+    IEnumerator ObstacleSpawnRoutine()
+    {
+        while (true) // while player alive
+        {
+            foreach (var item in obstacles)
+            {
+                Instantiate(_obstacles[i], _spawnPoint, );
+            }
+            yield return new WaitForSeconds(_objSpawnInterval);
+            
+        }
+    }
+  
 }
