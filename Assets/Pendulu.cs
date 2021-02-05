496e4c718fddf5bdfade1b11196e864208774786
kmiecikJob <76965636+kmiecikJob@users.noreply.github.com>
2021-02-05 16:32:54 +0100

    Penulum
    Swing axes from roof

---
+++ Assets/Pendulu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulu : MonoBehaviour
+{
+    [SerializeField, Range (0, 360)] private float angle = 90f;
+    [SerializeField, Range (0, 5)] private float speed = 2f;
+    [SerializeField, Range(0, 10)] private float startTime = 0;
+    private Quaternion start, end;
+
+    void Start()
+    {
+        start = PendulumRotation(angle);
+        end = PendulumRotation(-angle);
+    }
+
+    private void FixedUpdate()
+    {
+        startTime += Time.deltaTime;
+        transform.rotation = Quaternion.Lerp(start, end, (Mathf.Sin(startTime*speed + Mathf.PI / 2) + 1f) /2f);
+    }
+
+    private Quaternion PendulumRotation(float angle)
+    {
+        Quaternion pendulumRotation = transform.rotation;
+        var angleZ = pendulumRotation.eulerAngles.z + angle;
+
+        if(angleZ > 180)
+        {
+            angleZ -= 360;
+        } else if(angleZ < -180)
+        {
+            angleZ += 360;
+        }
+
+        pendulumRotation.eulerAngles = new Vector3(pendulumRotation.eulerAngles.x, pendulumRotation.eulerAngles.y, angleZ);
+        return pendulumRotation;
+    }
+
+    private void ResetTimer()
+    {
+        startTime = 0f;
+    }
+}
