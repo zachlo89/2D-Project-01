271ed93ce3c766dbcbd09d25e53eb5bd0da0e1d8
zachlo89 <zachlo89@gmail.com>
2021-02-09 13:00:47 -0800

    Revert "code clean up"
    This reverts commit 6cb768fa35a3622fdc589806b80707a496f24120.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    void Start()
+    {
+        Destroy(this.gameObject, 3.0f);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.up * 10 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private GameObject _bulletPrefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_bulletPrefab);
+        }
+    }
+}
