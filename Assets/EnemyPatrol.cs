cc565d642086bd4ea054e689af7039289aff026c
kmiecikJob <76965636+kmiecikJob@users.noreply.github.com>
2021-03-08 16:51:04 +0100

    Adding enemies and patrol script

---
+++ Assets/EnemyPatrol.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private Vector3 patrolPoint;
+    [SerializeField] private float speed;
+    private Vector3 startingPosition;
+    private bool shouldPatrol;
+    private bool toPatrolPoint; 
+
+    void Start()
+    {
+        toPatrolPoint = true;
+        shouldPatrol = true;
+        startingPosition = transform.position;
+    }
+
+    public void AggroOn(bool agro)
+    {
+        if(agro != shouldPatrol)
+        {
+            shouldPatrol = agro;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (shouldPatrol)
+        {
+            if (toPatrolPoint)
+            {
+                transform.LookAt(patrolPoint);
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                if(Vector3.Distance(transform.position, patrolPoint) < 5)
+                {
+                    toPatrolPoint = false;
+                }
+            } else
+            {
+                transform.LookAt(startingPosition);
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startingPosition) < 5)
+                {
+                    toPatrolPoint = true;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Enemy2Script.cs
+++ Assets/Scripts/Enemy2Script.cs
@@ -7,18 +7,35 @@ public class Enemy2Script : MonoBehaviour
     [SerializeField] private GameObject objectToFollow;
     private PlayerHealth playerHealth;
     [SerializeField] private float speed = 3f;
+    [SerializeField] private float aggroDistance = 500;
+    private EnemyPatrol patrol;
 
     private void Start()
     {
-        playerHealth = objectToFollow.GetComponent<PlayerHealth>();   
+        patrol = GetComponent<EnemyPatrol>();
+        if(objectToFollow == null)
+        {
+            objectToFollow = GameObject.FindGameObjectWithTag("Player");
+        }
+        playerHealth = objectToFollow.GetComponent<PlayerHealth>();
     }
 
     private void FixedUpdate()
     {
-        if(Vector3.Distance(transform.position, objectToFollow.transform.position) < 500 && playerHealth.currentHealth > 0)
+        if(Vector3.Distance(transform.position, objectToFollow.transform.position) < aggroDistance && playerHealth.currentHealth > 0)
         {
             transform.LookAt(objectToFollow.transform);
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            patrol.AggroOn(false);
+        } else
+        {
+            patrol.AggroOn(true);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, aggroDistance);
     }
 }
